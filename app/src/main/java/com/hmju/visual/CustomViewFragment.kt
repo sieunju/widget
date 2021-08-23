c7ec803c7f50e497a8de1f68c6405b7c77a4d519
sieunju <j.sieun73@gmail.com>
2021-08-23 17:49:54 +0900

    test: TestCode 추가

--- app/src/main/java/com/hmju/visual/CustomViewFragment.kt
+++ app/src/main/java/com/hmju/visual/CustomViewFragment.kt
@@ -6,10 +6,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import hmju.widget.view.CustomTextView
+import kotlinx.coroutines.*
 import java.net.MalformedURLException
 import java.net.URL
 
@@ -17,12 +15,21 @@ import java.net.URL
 class CustomViewFragment : Fragment(R.layout.fragment_custom_view) {
 
     private val TEMP_URL = "https://memo.qtzz.synology.me/resource/logo.png"
+    private val TEMP_STR = """
+        Ui 에서 실시간으로 진행률을 나타내야 할때 UiThread 로 처리하기에는 어려움이 있습니다.
+        그래서 주로 카메라 프리뷰에서 사용되는 SurfaceView 기반의 ProgressView 를 만들었습니다.
+        (UiThread 가 아닌 Worker Thread 에서 사용하셔도 Ui 표현이 가능합니다.)
+    """.trimIndent()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         GlobalScope.launch(Dispatchers.Main) {
             view.findViewById<AppCompatImageView>(R.id.imgThumb)
                 .setImageBitmap(imageBitmap(TEMP_URL))
+        }
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000)
+            view.findViewById<CustomTextView>(R.id.tvAuto).text = TEMP_STR
         }
     }
 
