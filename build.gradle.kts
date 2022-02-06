671d8dd7dde190cc1986de04e7b82ba1c0e2892f
sieunju <j.sieun73@gmail.com>
2022-02-06 22:27:19 +0900

    refactor: build.gradle Groovy -> KTS 리펙토링

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ widget/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(Apps.minSdkVersion)
+        targetSdkVersion(Apps.targetSdkVersion)
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+dependencies {
+    implementation(AndroidX.ktx)
+    implementation(AndroidX.appCompat)
+    implementation(AndroidX.constraintLayout)
+    implementation(AndroidX.material)
+    implementation(AndroidX.palette)
+    implementation(Coroutine.core)
+    implementation(Coroutine.android)
+    implementation(Rx.base)
+    implementation(Rx.android)
+    implementation(Rx.kotlin)
+
+    implementation(AndroidX.lifecycle)
+    implementation(AndroidX.liveData)
+}
