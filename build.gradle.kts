121f64c4a1f57b83dfffaf503e63841a69e60492
sieunju <j.sieun73@gmail.com>
2022-02-07 21:41:54 +0900

    feat: maven 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
 }
 
@@ -16,6 +17,7 @@ allprojects {
         mavenCentral()
         google()
     }
+    group = "com.github.sieunju"
 }
 
 tasks.register("clean", Delete::class) {
