447c38689a68b9dd7acbf7f595f0da09ae3e6189
sieunju <j.sieun73@gmail.com>
2022-02-07 22:38:01 +0900

    feat: 릴리즈노트 만드는 함수 및 task 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 buildscript {
     repositories {
         google()
@@ -22,4 +25,66 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+tasks.register("generateReleaseNote") {
+    getReleaseNote()
+}
+
+fun getCommand(command: String): String {
+    val os = ByteArrayOutputStream()
+    exec {
+        commandLine = command.split(" ")
+        standardOutput = os
+    }
+    return String(os.toByteArray())
+}
+
+/**
+ * 마지막 커밋한 메시지 가져와서 릴리즈노트에 입력 하기
+ */
+fun getReleaseNote() {
+    val lastTag = ByteArrayOutputStream().run {
+        project.exec {
+            commandLine("git describe --tags --abbrev=0".split(" "))
+            standardOutput = this@run
+        }
+        String(this.toByteArray()).trim()
+    }
+
+    if (lastTag.isEmpty()) {
+        println("Tag Message is Empty!")
+        return
+    }
+
+    File(project.rootDir.absolutePath, "release_note.txt").run {
+        parentFile.mkdir()
+        val buildDate = "Build Date ${
+            SimpleDateFormat(
+                "yyyy년 MM월 dd일 E요일 HH:mm:ss",
+                Locale.KOREAN
+            ).format(Date())
+        }"
+
+        val version = "Version Name: ${Apps.versionName}"
+        val branch = "Branch: ${getCommand("git rev-parse --abbrev-ref HEAD")}"
+        val msg = "Message: ${getCommand("git rev-list --format=%B --max-count=1 HEAD")}"
+        val author = "Author: ${getCommand("git log -1 --pretty=format:%an")}"
+
+        printWriter().use {
+            it.println(buildDate)
+            it.println(version)
+            it.println(branch)
+            it.println(msg)
+            it.println(author)
+        }
+
+        println(buildDate)
+        println(version)
+        println(branch)
+        println(msg)
+        println(author)
+
+        println("ReleaseNote Write Success")
+    }
 }
