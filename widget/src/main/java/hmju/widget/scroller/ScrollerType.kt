4005876f58fe45256804d5752b80a894b6651bce
sieunju <j.sieun73@gmail.com>
2022-02-07 15:31:40 +0900

    feat: CustomLinearScroller 추가

---
+++ widget/src/main/java/hmju/widget/scroller/ScrollerType.kt
@@ -0,0 +1,10 @@
+package hmju.widget.scroller
+
+/**
+ * Description : CustomLinearScroller Enum Class
+ *
+ * Created by juhongmin on 2022/02/03
+ */
+enum class ScrollerType {
+    START, END, CENTER
+}
