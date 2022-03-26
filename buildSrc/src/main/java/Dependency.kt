20686c47f5008e61f8eacbf04bef82edce52451c
sieunju <j.sieun73@gmail.com>
2022-03-26 18:02:29 +0900

    fix: 라이브러리 배포 안되는 이슈 처리 - 의심가는 부분에 수정 처리해서 적용 버전 업데이트

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -4,8 +4,8 @@ object Apps {
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 19
-    const val versionName = "0.0.19"
+    const val versionCode = 20
+    const val versionName = "0.0.20"
 }
 
 object Versions {
