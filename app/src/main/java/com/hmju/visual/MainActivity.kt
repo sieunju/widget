fa9fc83672fe68dd73107755fa3bd99803a03568
sieunju <j.sieun73@gmail.com>
2022-12-30 18:43:32 +0900

    feat: Fragment 애니메이션 추가 - 불필요한 클래스 제거

--- app/src/main/java/com/hmju/visual/MainActivity.kt
+++ app/src/main/java/com/hmju/visual/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hmju.visual
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -30,6 +29,12 @@ class MainActivity : AppCompatActivity() {
     companion object {
         fun FragmentManager.moveToFragment(targetFragment: KClass<out Fragment>) {
             beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in_right,
+                    R.anim.slide_out_left,
+                    R.anim.slide_in_left,
+                    R.anim.slide_out_right
+                )
                 replace(R.id.fragment, targetFragment.java.newInstance())
                 addToBackStack(null)
                 commit()
