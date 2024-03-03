0c93c1fbd7cdaa8f4e55b316433731c7b906d8a3
sieunju <j.sieun73@gmail.com>
2024-03-03 22:46:14 +0900

    feat: DynamicCoordinatorLayout 추가

--- app/src/main/java/com/hmju/visual/ui/coordinator/DynamicCoordinatorActivity.kt
+++ app/src/main/java/com/hmju/visual/ui/coordinator/DynamicCoordinatorActivity.kt
@@ -1,8 +1,13 @@
 package com.hmju.visual.ui.coordinator
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.google.android.material.appbar.AppBarLayout
 import com.hmju.visual.R
+import timber.log.Timber
 
 /**
  * Description :
@@ -10,8 +15,32 @@ import com.hmju.visual.R
  * Created by juhongmin on 3/3/24
  */
 internal class DynamicCoordinatorActivity : AppCompatActivity() {
+
+    val Int.dp: Int
+        get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this.toFloat(),
+            Resources.getSystem().displayMetrics
+        ).toInt()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.a_dynamic_coordinator)
+        val llHeader = findViewById<LinearLayoutCompat>(R.id.llHeader)
+        findViewById<AppBarLayout>(R.id.abl).addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val totalRange = appBarLayout.totalScrollRange
+            val offset = Math.abs(verticalOffset)
+            Timber.d("Scroll Offset $totalRange $offset")
+
+            val diffOffset = if (offset <= 50.dp) {
+                -offset
+            } else {
+                val aOffset = Math.min(50.dp, totalRange.minus(offset))
+                val bOffset = 50.dp.minus(aOffset)
+                -50.dp
+            }
+            llHeader.translationY = diffOffset.toFloat()
+        }
+
     }
 }
