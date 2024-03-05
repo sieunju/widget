56b4825ed80c0d2daa8ef616dd0046532c17fe26
sieunju <j.sieun73@gmail.com>
2024-03-05 11:06:09 +0900

    feat: 동적 콘텐츠 로직 계산 1차 적용

--- app/src/main/java/com/hmju/visual/ui/coordinator/DynamicCoordinatorActivity.kt
+++ app/src/main/java/com/hmju/visual/ui/coordinator/DynamicCoordinatorActivity.kt
@@ -1,6 +1,5 @@
 package com.hmju.visual.ui.coordinator
 
-import android.annotation.SuppressLint
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.TypedValue
@@ -9,7 +8,6 @@ import androidx.databinding.DataBindingUtil
 import com.google.android.material.appbar.AppBarLayout
 import com.hmju.visual.R
 import com.hmju.visual.databinding.ADynamicCoordinatorBinding
-import timber.log.Timber
 import kotlin.math.abs
 
 
@@ -27,66 +25,67 @@ internal class DynamicCoordinatorActivity : AppCompatActivity() {
             Resources.getSystem().displayMetrics
         ).toInt()
 
+    val Float.dp: Float
+        get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this,
+            Resources.getSystem().displayMetrics
+        )
+
     enum class State {
         EXPANDED,
-        COLLAPSED,
-        HALF_COLLAPSED
+        COLLAPSED
     }
 
     private lateinit var binding: ADynamicCoordinatorBinding
 
-    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.a_dynamic_coordinator)
-        val statusBarHeight = getStatusBarHeight()
-        Timber.d("StatusBar $statusBarHeight")
+        binding = DataBindingUtil.setContentView(this, R.layout.a_dynamic_coordinator)
         binding.abl.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
             var currentState: State? = null
-            var currentOffset = 0
             override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-                val totalRange = appBarLayout.totalScrollRange // 750
-                val offset = Math.abs(verticalOffset)
-                val appBarTop = appBarLayout.top
-                val appBarBottom = appBarLayout.bottom
-                val appBarHeight = if (appBarTop > appBarBottom) {
-                    appBarTop.plus(appBarBottom)
-                } else {
-                    appBarBottom.plus(appBarTop)
-                }
-
+                val totalRange = appBarLayout.totalScrollRange
+                val offset = abs(verticalOffset)
                 if (offset == 0) {
                     currentState = State.EXPANDED
                 } else if (offset >= totalRange) {
                     currentState = State.COLLAPSED
                 }
 
-                // 아직 안걸쳐짐
-                if (appBarHeight > (-50).dp) {
-                    binding.llHeader.translationY = -(Math.min(50.dp,offset)).toFloat()
-                    Timber.d("아직 안걸쳐짐 ${appBarHeight} $offset ${binding.llHeader.translationY}")
+                // Timber.d("ScrollOffset $offset")
+                if (offset <= totalRange.minus(100.dp) && currentState == State.EXPANDED) {
+                    val ratioY = getExpandRatioY(offset, 0, totalRange.minus(100.dp), 0, 50.dp)
+                    binding.llHeader.translationY = -ratioY
                 } else {
-                    // 걸쳐진 상태
-                    // -150.dp ~ -50.dp
-                    if (currentState == State.COLLAPSED) {
-                        val newValue = adjustRange(abs(appBarHeight), 50.dp,150.dp,0,50.dp)
-                        binding.llHeader.translationY = -newValue
-                        Timber.d("걸쳐진 상태 $appBarHeight $newValue  ${binding.nsv.top}")
+                    if (currentState == State.COLLAPSED && offset in totalRange.minus(50.dp)..totalRange) {
+                        val ratioY = getCollapsedRatioY(offset)
+                        binding.llHeader.translationY = ratioY
                     }
                 }
             }
         })
     }
 
-    fun adjustRange(value: Int, min1: Int, max1: Int, min2: Int, max2: Int): Float {
-        // 현재 값의 비율 계산
-        val ratio = (value.toFloat() - min1.toFloat()) / (max1.toFloat() - min1.toFloat())
-        return ratio * (max2 - min2) + min2
+    fun getCollapsedRatioY(value: Int): Float {
+        if (value !in 200.dp..250.dp) {
+            return (-50F).dp
+        }
+        // 비율 계산
+        // 접힌 상태에서 펼쳐졌을때와 접힐떄 위치, S:200 E:250
+        val ratio = (value.toFloat() - 200F.dp) / (50F.dp)
+        // 헤더 이동 S:0, E: -50
+        return ratio * (-50F).dp
     }
 
-    @SuppressLint("InternalInsetResource")
-    fun getStatusBarHeight(): Int {
-        val id = resources.getIdentifier("status_bar_height", "dimen", "android")
-        return if (id > 0) resources.getDimensionPixelSize(id) else 0
+    fun getExpandRatioY(
+        value: Int,
+        min1: Int,
+        max1: Int,
+        min2: Int,
+        max2: Int
+    ): Float {
+        val ratio = (value.toFloat() - min1.toFloat()) / (max1 - min1).toFloat()
+        return ratio * (max2 - min2) + min2
     }
 }
